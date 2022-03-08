90db7af515ef7046d88da98f4dbf211a0f8b163c
조은지 <dmswl_0311@naver.com>
2022-03-08 14:51:30 +0900

    feat: spring boot 프로젝트 생성

---
+++ backend/soboksobok/src/main/java/com/soboksobok/soboksobok/SoboksobokApplication.java
@@ -0,0 +1,13 @@
+package com.soboksobok.soboksobok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoboksobokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoboksobokApplication.class, args);
+	}
+
+}
