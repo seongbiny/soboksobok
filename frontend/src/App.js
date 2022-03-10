f44f1979514df7a38813001e03c9c8083f8495c5
seongbiny <s30ngb1n@gmail.com>
2022-03-10 14:49:32 +0900

    fix: router error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelfareDetail from "./pages/WelfareDetail";
-import NavBar from './components/Navbar.js';
-import Main from './pages/Main.jsx';
+import NavBar from "./components/Navbar.js";
+import Main from "./pages/Main.jsx";
 
 function App() {
   return (
-    <Router>
+    <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Main />} />
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/welfare" element={<WelfareDetail />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
