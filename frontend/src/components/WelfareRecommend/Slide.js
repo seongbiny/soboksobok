c4a0caf3846802156dc87b80c2d1d0746f677375
seongbiny <s30ngb1n@gmail.com>
2022-03-14 11:30:09 +0900

    [S06P22C205-54] Feat: card swiper 수정

--- frontend/src/components/WelfareRecommend/Slide.js
+++ frontend/src/components/WelfareRecommend/Slide.js
@@ -1,31 +1,51 @@
 import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Pagination, Autoplay, Navigation } from "swiper/core";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import WelfareCard from "../WelfareCard";
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-};
+SwiperCore.use([Pagination, Autoplay, Navigation]);
 
 function Slide() {
   return (
-    <div>
-      <h2>추천복지</h2>
-      <Slider {...settings}>
-        <WelfareCard />
-        <WelfareCard />
-        <WelfareCard />
-        <WelfareCard />
-        <WelfareCard />
-        <WelfareCard />
-        <WelfareCard />
-      </Slider>
-    </div>
+    <main className="ExampleComponent">
+      <div className="main-wrap">
+        <h1>USER님에게 추천하는 복지</h1>
+        <Swiper
+          style={{
+            width: "70vw",
+            height: "40vh",
+            backgroundColor: "#FFF5F1",
+            borderRadius: "12px",
+          }}
+          spaceBetween={8}
+          slidesPerView={3}
+          initialSlide={1}
+          navigation
+          scrollbar={{ draggable: true }}
+          pagination={{
+            clickable: true,
+          }}
+          loop
+          autoplay={{ delay: 3000 }}
+        >
+          <SwiperSlide>
+            <WelfareCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <WelfareCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <WelfareCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <WelfareCard />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </main>
   );
 }
 export default Slide;
--- frontend/src/pages/WelfareRecommend.jsx
+++ frontend/src/pages/WelfareRecommend.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import Chart from "../components/WelfareRecommend/Chart";
 import LineChart from "../components/WelfareRecommend/LineChart";
-import styled from 'styled-components';
 import Slide from "../components/WelfareRecommend/Slide";
+import styled from 'styled-components';
+import { Avatar } from "@mui/material";
 
 const StyledContainer = styled.div`
-
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+    row-gap:30px;
+    justify-content: center;
+`;
+const StyledBox = styled.div`
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 3;
 `;
 
 function WelfareRecommend(){
     return(
         <StyledContainer>
+            <Avatar />
             <Chart />
             <LineChart />
-            <Slide />
+            <StyledBox>
+                <Slide />
+            </StyledBox>
         </StyledContainer>
     )
 }
