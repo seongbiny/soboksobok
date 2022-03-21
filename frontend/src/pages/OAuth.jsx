077c5b0407f8f4832785de2c0b9efd980bc38743
이현정 <h.jung0409@daum.net>
2022-03-21 09:39:33 +0900

    chore: 변경사항 추가

--- frontend/src/pages/OAuth.jsx
+++ frontend/src/pages/OAuth.jsx
@@ -6,7 +6,9 @@ import { useNavigate } from 'react-router-dom';
 
 function Auth() {
   const REST_API_KEY = 'b0c4483210e0ea0db8f56255adbeeda5';
+  //카카오 로그인에서 사용할 OAuth Redirect URI를 설정합니다
   const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
+  //토큰 발급 시, 보안을 강화하기 위해 Client Secret을 사용할 수 있습니다. (REST API인 경우에 해당)
   const CLIENT_SECRET = 'pe7y4NArlDuQpJYXu8nxNFitPddPonFY';
 
   // calllback으로 받은 인가코드
