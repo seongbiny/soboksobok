23fca8cd49541ea86c8b81162df69945d7902054
조은지 <dmswl_0311@naver.com>
2022-03-23 14:27:07 +0900

    fix: QNA 게시글 api user 받는 부분 수정

--- backend/soboksobok/src/main/java/com/soboksobok/soboksobok/controller/QnaController.java
+++ backend/soboksobok/src/main/java/com/soboksobok/soboksobok/controller/QnaController.java
@@ -42,12 +42,14 @@ public class QnaController {
 
     @GetMapping("/mine")
     @ApiOperation(value="나의 qna 조회",notes="내가 작성한 모든 qna를 조회합니다.")
-    public ResponseEntity<List<QnaDto>> getMyQna(@RequestParam Long userId) throws Exception{
+    public ApiResponse getMyQna() throws Exception{
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(principal.getUsername());
+        log.info("유저 확인: {}"+user.getUsername());
+        Long userId=user.getUserSeq();
         List<QnaDto> list=service.getMyQna(userId);
-        ResponseEntity<List<QnaDto>> res=new ResponseEntity(list, HttpStatus.OK);
-        log.info("userId: "+userId);
-        log.info("qna 개수 확인: "+list.size());
-        return res;
+        log.info("qna 개수 확인: {}"+list.size());
+        return ApiResponse.success("success",list);
     }
     @GetMapping("/{qna_id}")
     @ApiOperation(value="qna 상세 조회",notes="qna를 상세 조회합니다.(user 상관없이)")
@@ -59,48 +61,48 @@ public class QnaController {
 
     @GetMapping("/mine/{qna_id}")
     @ApiOperation(value="나의 qna 상세 조회",notes="내가 등록한 qna를 상세 조회합니다.")
-    public ApiResponse getMyQnaDetail(@PathVariable("qna_id") Long qna_id, @RequestParam Long userId) throws Exception{
+    public ApiResponse getMyQnaDetail(@PathVariable("qna_id") Long qna_id) throws Exception{
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(principal.getUsername());
+        log.info("유저 확인: {}"+user.getUsername());
+        Long userId=user.getUserSeq();
         QnaDto qna=service.getMyQnaDetail(qna_id,userId);
         List<CommentResDto> comments = qna.getComments(); //댓글
-//        ResponseEntity<QnaDto> res=new ResponseEntity(qna, HttpStatus.OK);
-//        log.info("userId: "+userId);
         return ApiResponse.success("success",qna);
     }
 
     @PostMapping("/mine")
     @ApiOperation(value="qna 등록",notes="qna를 등록합니다.")
-    public ApiResponse createMyQna(@RequestBody WriteQnaDto dto, @RequestParam("userId") Long user_seq) throws Exception{
-//        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User user = (User)principal;
-        // 받은 아이디와 로그인 한 유저의 아이디가 같을 때만 실행
-        // 로그인 한 유저 받아야 함. 1은 임시값
-//        if(user.getUserSeq()!=user_id) return ApiResponse.fail();
-        if(Long.valueOf(1)!=user_seq) return ApiResponse.fail();
+    public ApiResponse createMyQna(@RequestBody WriteQnaDto dto) throws Exception{
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(principal.getUsername());
+        log.info("유저 확인: {}"+user.getUsername());
+        Long userId=user.getUserSeq();
         QnaDto qnaDto=QnaDto.of(dto);
-        QnaDto qna=service.createMyQna(qnaDto,user_seq);
+        QnaDto qna=service.createMyQna(qnaDto,userId);
         return ApiResponse.success("success",qna);
     }
 
     @DeleteMapping("/mine/{qna_id}")
     @ApiOperation(value="qna 삭제",notes="qna를 삭제합니다.")
-    public ApiResponse deleteMyQna(@PathVariable("qna_id") Long qna_id, @RequestParam("userId") Long userId) throws Exception{
+    public ApiResponse deleteMyQna(@PathVariable("qna_id") Long qna_id) throws Exception{
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(principal.getUsername());
+        log.info("유저 확인: {}"+user.getUsername());
+        Long userId=user.getUserSeq();
         String result=service.deleteMyQna(qna_id,userId);
         return ApiResponse.success("success",result);
     }
 
     @PatchMapping("/mine/{qna_id}")
     @ApiOperation(value="qna 수정",notes="qna를 수정합니다. content와 title에 수정된 내용을 넣어줍니다. 만약, 수정된 내용이 없다면 원래 내용을 넣어주세요.")
-    public  ApiResponse updateMyQna(@PathVariable("qna_id") Long qna_id, @RequestParam("userId") Long user_seq, @RequestBody WriteQnaDto dto) throws Exception{
-//        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        User user = (User)principal; // 현재 로그인 한 유저
-        // 받은 아이디와 로그인 한 유저의 아이디가 같을 때만 실행
-        // 로그인 한 유저 받아야 함. 1은 임시값
-//        if(user.getUserSeq()!=user_id) return ApiResponse.fail();
-        if(Long.valueOf(1)!=user_seq) return ApiResponse.fail();
+    public  ApiResponse updateMyQna(@PathVariable("qna_id") Long qna_id, @RequestBody WriteQnaDto dto) throws Exception{
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.getUser(principal.getUsername());
+        log.info("유저 확인: {}"+user.getUsername());
+        Long userId=user.getUserSeq();
         QnaDto qnaDto=QnaDto.of(dto);
-        String result=service.updateMyQna(qna_id,user_seq,qnaDto);
+        String result=service.updateMyQna(qna_id,userId,qnaDto);
         return ApiResponse.success("success",result);
     }
 }
