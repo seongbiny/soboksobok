48417655cc1c736d181758bdd2ac6f8181d509c7
송상빈 <ssbin407@naver.com>
2022-03-30 17:33:11 +0900

    chore : 복지 데이터 입력 관련 수정

--- backend/soboksobok/src/main/java/com/soboksobok/soboksobok/domain/welfare/Similarwelfare.java
+++ backend/soboksobok/src/main/java/com/soboksobok/soboksobok/domain/welfare/Similarwelfare.java
@@ -1,9 +1,13 @@
 package com.soboksobok.soboksobok.domain.welfare;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 public class Similarwelfare {
@@ -11,5 +15,4 @@ public class Similarwelfare {
     @Id
     private Long welfare_id;
 
-    private List<Integer>
 }
