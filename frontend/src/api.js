c58ed1d00c96fc22fd1134f606459089567386f1
seongbiny <s30ngb1n@gmail.com>
2022-04-01 18:03:09 +0900

    api fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: "http://j6c205.p.ssafy.io:8000",
   headers: {
-    'Content-type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTION',
-    'Access-Control-Allow-Headers':
-      'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
-    'Access-Control-Expose-Headers': 'Content-Length,Content-Range',
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTION",
+    "Access-Control-Allow-Headers":
+      "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization",
+    "Access-Control-Expose-Headers": "Content-Length,Content-Range",
   },
 });
 
 export default function getAxios() {
-  const token = localStorage.getItem('jwtToken');
-  Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  const token = localStorage.getItem("jwtToken");
+  Axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   return Axios;
 }
