6b30e9b007d7adba3636f1343c16598d4de4297a
seongbiny <s30ngb1n@gmail.com>
2022-04-06 01:20:40 +0900

    [S06P22C205-284] feat: pagination 구현 !

--- frontend/src/components/Search/ResultBoard.js
+++ frontend/src/components/Search/ResultBoard.js
@@ -4,37 +4,98 @@ import Table from "react-bootstrap/Table";
 import { getAxios } from "../../api.js";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Pagination from "./Pagination.js";
+import PaginationBtn from "./PaginationBtn";
+import { paginate } from "./paginate";
+import _ from "lodash";
 
 function ResultBoard() {
   const axios = getAxios();
-  const [result, setResult] = useState([]);
   const { keyword } = useSelector(state => state.change);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
   const navigate = useNavigate();
-  const indexOfLast = currentPage * postsPerPage;
-  const indexOfFirst = indexOfLast - postsPerPage;
 
-  const [currPage, setCurrPage] = useState(0);
-
-  function currentPost(tmp) {
-    let currentPosts = 0;
-    currentPosts = tmp.slice(indexOfFirst, indexOfLast);
-    return currentPosts;
-  }
-
-  const handlePageChange = currPage => {
-    setCurrPage(currPage);
+  const getData = () => {
+    const welfares = [
+      { index: 1, title: "사과", date: "2022.03.22" },
+      { index: 2, title: "딸기", date: "2022.03.22" },
+      { index: 3, title: "토마토", date: "2022.03.22" },
+      { index: 4, title: "메론", date: "2022.03.22" },
+      { index: 5, title: "바나나", date: "2022.03.22" },
+      { index: 6, title: "바나나", date: "2022.03.22" },
+      { index: 7, title: "바나나", date: "2022.03.22" },
+      { index: 8, title: "사과", date: "2022.03.22" },
+      { index: 9, title: "딸기", date: "2022.03.22" },
+      { index: 10, title: "토마토", date: "2022.03.22" },
+      { index: 11, title: "메론", date: "2022.03.22" },
+      { index: 12, title: "바나나", date: "2022.03.22" },
+      { index: 13, title: "바나나", date: "2022.03.22" },
+      { index: 14, title: "바나나", date: "2022.03.22" },
+      { index: 15, title: "바나나", date: "2022.03.22" },
+      { index: 16, title: "바나나", date: "2022.03.22" },
+      { index: 17, title: "바나나", date: "2022.03.22" },
+      { index: 18, title: "바나나", date: "2022.03.22" },
+      { index: 19, title: "사과", date: "2022.03.22" },
+      { index: 20, title: "딸기", date: "2022.03.22" },
+      { index: 21, title: "토마토", date: "2022.03.22" },
+      { index: 22, title: "메론", date: "2022.03.22" },
+      { index: 23, title: "바나나", date: "2022.03.22" },
+      { index: 24, title: "바나나", date: "2022.03.22" },
+      { index: 25, title: "바나나", date: "2022.03.22" },
+      { index: 26, title: "바나나", date: "2022.03.22" },
+      { index: 27, title: "바나나", date: "2022.03.22" },
+      { index: 28, title: "바나나", date: "2022.03.22" },
+      { index: 29, title: "바나나", date: "2022.03.22" },
+      { index: 30, title: "사과", date: "2022.03.22" },
+      { index: 31, title: "딸기", date: "2022.03.22" },
+      { index: 32, title: "메론", date: "2022.03.22" },
+      { index: 33, title: "바나나", date: "2022.03.22" },
+      { index: 34, title: "바나나", date: "2022.03.22" },
+      { index: 35, title: "바나나", date: "2022.03.22" },
+      { index: 36, title: "바나나", date: "2022.03.22" },
+      { index: 37, title: "바나나", date: "2022.03.22" },
+      { index: 38, title: "바나나", date: "2022.03.22" },
+      { index: 39, title: "바나나", date: "2022.03.22" },
+      { index: 40, title: "사과", date: "2022.03.22" },
+      { index: 41, title: "딸기", date: "2022.03.22" },
+      { index: 42, title: "토마토", date: "2022.03.22" },
+      { index: 43, title: "메론", date: "2022.03.22" },
+      { index: 44, title: "바나나", date: "2022.03.22" },
+      { index: 45, title: "바나나", date: "2022.03.22" },
+      { index: 46, title: "바나나", date: "2022.03.22" },
+      { index: 47, title: "사과", date: "2022.03.22" },
+      { index: 48, title: "딸기", date: "2022.03.22" },
+      { index: 49, title: "토마토", date: "2022.03.22" },
+      { index: 50, title: "메론", date: "2022.03.22" },
+      { index: 51, title: "바나나", date: "2022.03.22" },
+      { index: 52, title: "바나나", date: "2022.03.22" },
+      { index: 53, title: "바나나", date: "2022.03.22" },
+      { index: 54, title: "바나나", date: "2022.03.22" },
+      { index: 55, title: "바나나", date: "2022.03.22" },
+    ];
+    return welfares;
   };
+
+  const [welfares, setWelfares] = useState({
+    data: getData(),
+    pageSize: 10, // 한 페이지에 보여줄 데이터 개수
+    currentPage: 1, // 현재 활성화된 페이지 위치
+  });
+
+  const handlePageChange = page => {
+    setWelfares({ ...welfares, currentPage: page });
+    console.log(page);
+  };
+
+  const { data, pageSize, currentPage } = welfares;
+  const pagedWelfares = paginate(data, currentPage, pageSize); // 페이지 별로 데이터가 속한 배열을 얻어옴
 
   useEffect(() => {
     const fetchSearch = async () => {
       try {
         const request = await axios.get(`/api/welfare/search/${keyword}`);
         navigate(`/search?keyword=${keyword}`);
-        setResult(request.data);
-        // console.log(request.data);
+        // setResult(request.data);
+        setWelfares({ ...welfares, data: request.data });
+        console.log(request.data);
       } catch (err) {
         console.log(err);
       }
@@ -46,7 +107,8 @@ function ResultBoard() {
     navigate(`/welfare/${id}`);
   };
 
-  const { length: count } = result;
+  // const { length: count } = result;
+  const { length: count } = data;
   if (count === 0) {
     return <StyledNo>검색 정보가 없습니다.</StyledNo>;
   }
@@ -63,24 +125,24 @@ function ResultBoard() {
             </tr>
           </thead>
           <tbody>
-            {currentPost(result).map(welfare => (
+            {pagedWelfares.map(welfare => (
               <tr key={welfare[0]}>
-                <td className="text-center">{welfare[0]}</td>
-                <StyledTd onClick={e => onClick(welfare[0])}>
-                  {welfare[1]}
-                </StyledTd>
-                <td className="text-center">{welfare[2]}</td>
+                <td>{welfare[0]}</td>
+                <td>{welfare[1]}</td>
+                <td>{welfare[2]}</td>
               </tr>
             ))}
           </tbody>
         </Table>
       </StyledTable>
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={result.length}
-        paginate={setCurrentPage}
-        currentPage={currentPage}
-      />
+      <StyledPage>
+        <PaginationBtn
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </StyledPage>
     </StyledBoard>
   );
 }
@@ -95,6 +157,11 @@ const StyledNo = styled.div`
   line-height: 50vh;
   text-align: center;
   font-weight: bold;
+`;
+
+const StyledPage = styled.div`
+  margin: 0 auto;
+  margin-top: 10px;
 `;
 
 const StyledBoard = styled.div`
