c06f9b79a830baba71c3a47e5d1ad8dd64da7118
seongbiny <s30ngb1n@gmail.com>
2022-04-05 15:06:04 +0900

    login

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const NotFound = () => {
-    return (
-        <div>
-            <h1>404 Not Found</h1>
-        </div>
-    )
-}
-export default NotFound;
+// const NotFound = () => {
+//     return (
+//         <div style={{height:"50vh"}}>
+//             <h1>404 Not Found</h1>
+//             <img src="drawKit/SVG/DrawKit (10).svg" width="100%" alt="404" />
+//         </div>
+//     )
+// }
+// export default NotFound;
