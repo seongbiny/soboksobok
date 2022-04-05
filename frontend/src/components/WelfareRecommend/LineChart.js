82915cf9c2d872e65e8cc61a9eae5253a1010851
seongbiny <s30ngb1n@gmail.com>
2022-04-06 00:23:04 +0900

    sb

--- frontend/src/components/WelfareRecommend/LineChart.js
+++ frontend/src/components/WelfareRecommend/LineChart.js
@@ -4,32 +4,41 @@ import { Bar } from "react-chartjs-2";
 import { useEffect, useState } from "react";
 import { getAxios } from "../../api";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import {
+  welDataName,
+  welDataContent,
+  welDataId,
+} from "../../reducers/welData.js";
 
 const Container = styled.div`
   max-width: 800px;
   height: 80%;
-  padding: 3vh;
+  // padding: 3vh;
 `;
 
 function Chart() {
   const axios = getAxios();
   const [label, setLabel] = useState([]);
   const [datas, setDatas] = useState([]);
+  const dispatch = useDispatch();
 
   const data = {
     // labels: ["1번", "2번", "3번", "4번", "5번"],
     labels: label,
     datasets: [
       {
-        label: "받을 수 있는 지원유형",
+        label: "인기 복지 조회 순",
         // data: [65, 59, 80, 81, 56, 55, 40],
         data: datas,
+        axis: "y",
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+          "rgba(255, 205, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
           // "rgba(153, 102, 255, 0.2)",
           // "rgba(201, 203, 207, 0.2)",
         ],
@@ -39,6 +48,7 @@ function Chart() {
           "rgb(255, 205, 86)",
           "rgb(75, 192, 192)",
           "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
           // "rgb(153, 102, 255)",
           // "rgb(201, 203, 207)",
         ],
@@ -56,21 +66,24 @@ function Chart() {
         beginAtZero: true,
       },
     },
+    indexAxis: "y",
     maintainAspectRatio: false,
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("/api/welfare/recommend/purpose");
-        console.log(res.data);
-        // let wel = res.data.map(a => a.welfare_service_name);
-        // await setLabel(wel);
-        // let view = res.data.map(a => a.welfare_view);
-        // await setDatas(view);
-        let wel = res.data;
-        await setLabel(Object.keys(wel).slice(0, 6));
-        await setDatas(Object.values(wel).slice(0, 6));
+        const request = await axios.get("/api/welfare/recommend/grouppopular");
+        console.log(request.data);
+        let wel = request.data.map(a => a.welfare_service_name);
+        await setLabel(wel);
+        await dispatch(welDataName(wel));
+        let view = request.data.map(a => a.welfare_view);
+        await setDatas(view);
+        let welNum = request.data.map(a => a.welfare_id);
+        await dispatch(welDataId(welNum));
+        let con = request.data.map(a => a.welfare_service_content);
+        await dispatch(welDataContent(con));
       } catch (err) {
         console.log(err);
       }
