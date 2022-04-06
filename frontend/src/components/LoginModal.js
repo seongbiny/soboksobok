54454b0f9446244382ccc70852ad9fc40af81f5a
이현정 <h.jung0409@daum.net>
2022-04-06 21:16:49 +0900

    [S06P22C205-287] chore: 인기 검색어 news ticker 디자인 수정

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -7,7 +7,7 @@ function LoginModal() {
   return (
     <StyledContainer>
       <Modal.Dialog>
-        <Modal.Body closeButton>
+        <Modal.Body>
           <p>로그인이 필요한 페이지입니다.</p>
         </Modal.Body>
 
